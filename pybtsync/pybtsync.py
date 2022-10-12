29dfb2153e322f7eec3cff966d796ecf202bd26b
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-12 21:24:26 +0000

    Adding tarfile member sanitization to extractall()

--- pybtsync/pybtsync.py
+++ pybtsync/pybtsync.py
@@ -109,7 +109,26 @@ class BTSync_process():
             tar_gz.write(response.content)
             tar_gz.seek(0)
             with tarfile.open(mode='r:gz', fileobj=tar_gz) as btsync:
-                btsync.extractall(btsync_dir)
+                def is_within_directory(directory, target):
+                    
+                    abs_directory = os.path.abspath(directory)
+                    abs_target = os.path.abspath(target)
+                
+                    prefix = os.path.commonprefix([abs_directory, abs_target])
+                    
+                    return prefix == abs_directory
+                
+                def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                
+                    for member in tar.getmembers():
+                        member_path = os.path.join(path, member.name)
+                        if not is_within_directory(path, member_path):
+                            raise Exception("Attempted Path Traversal in Tar File")
+                
+                    tar.extractall(path, members, numeric_owner=numeric_owner) 
+                    
+                
+                safe_extract(btsync, btsync_dir)
             
         return btsync_name 
 
